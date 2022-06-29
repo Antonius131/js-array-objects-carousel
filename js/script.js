92bd38563f37732016e2022acc23bf6365ac4c70
Antonius131 <antomendetta@gmail.com>
2022-06-29 16:50:47 +0200

    clean indented code

--- js/script.js
+++ js/script.js
@@ -1,39 +1,34 @@
 const images = [
-   {
-       url: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg',
-       title: 'Svezia',
-       description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+   { 
+      url: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg',
+      title: 'Svezia',
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
    },
 
    {
-       url: 'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg',
-       title: 'Perù',
-       description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+      url: 'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg',
+      title: 'Perù',
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
    },
 
    {
-       url: 'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c',
-       title: 'Chile',
-       description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+      url: 'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c',
+      title: 'Chile',
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
    },
+
    {
-       url: 'https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg',
-       title: 'Argentina',
-       description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+      url: 'https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg',
+      title: 'Argentina',
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
    },
+   
    {
-       url: 'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop',
-       title: 'Colombia',
-       description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
-   },
+      url: 'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop',
+      title: 'Colombia',
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+   }
 ];
-
-
-/**
- * 
- *    - dato l'array di oggetti 'images', popolare l'html con gli oggetti inseriti all'interno del markup pensato
- *    - al click sui pulsanti "prev" e "next", visualizzarsi la prossima immagine, popolata con gli elementi degli oggetti
- */
 
 
 // put object array in DOM
@@ -57,12 +52,11 @@ let imgIndex = 0;
 imgList[imgIndex].classList.add('active');
 
 
+
+
 // add eventListener on buttons
 const nextBtn = document.getElementById('next-btn');
 const prevBtn = document.getElementById('prev-btn');
-
-console.log(imgList);
-console.log(imgIndex);
 
 nextBtn.addEventListener('click', function() {
    imgList[imgIndex].classList.remove('active');
@@ -74,8 +68,6 @@ nextBtn.addEventListener('click', function() {
    }
 
    imgList[imgIndex].classList.add('active');
-
-   console.log(imgIndex)
 
 });
 
@@ -89,7 +81,5 @@ prevBtn.addEventListener('click', function() {
    }
 
    imgList[imgIndex].classList.add('active');
-
-   console.log(imgIndex);
 
 });
